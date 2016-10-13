345602c3852832d2c09238a19a30574cfe7dd56b
Tim Foley <thetimfoley@gmail.com>
2016-10-13 10:10:45 -0400

    updated controllers

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,47 @@
 class CharactersController < ApplicationController
+  # replace Character
+  # replace character
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create(character_params)
+
+    redirect_to character_path(@character)
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+
+    redirect_to character_path(@character)
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
+  end
+
+  # strong params
+  private
+  def character_params
+    params.require(:character).permit(:name, :img_url, :quote, :bio, :house)
+  end
+
+
 end
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,45 @@
 class HousesController < ApplicationController
+  # replace House
+  # replace house
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create(house_params)
+
+    redirect_to house_path(@house)
+  end
+
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+
+    redirect_to house_path(@house)
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+
+    redirect_to houses_path
+  end
+
+  # strong params
+  private
+  def house_params
+    params.require(:house).permit(:name, :words, :seat, :region, :img_url)
+  end
 end
